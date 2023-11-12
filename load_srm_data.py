a9ba9b87f8f1f91657e8ce92afa55ae5a5d90a0f
unknown <chris.s.iyer@gmail.com>
2023-11-12 14:21:17 -0800

    srm data script

--- load_srm_data.py
+++ load_srm_data.py
@@ -15,4 +15,16 @@ for task in tasks:
     data_srm = srm_transform(data, subjects)
     print(f'srm\'d  {task}')
 
-    np.savez(f'/scratch/users/csiyer/decoding_outputs/srm_data_{task}.npz', data=data_srm, labels=labels, subjects=subjects)
+    np.savez(f'/scratch/users/csiyer/decoding_outputs/srm_data_{task}.npz', *data_srm)
+    np.savez(f'/scratch/users/csiyer/decoding_outputs/labels_{task}.npz', *labels)
+    np.save(f'/scratch/users/csiyer/decoding_outputs/subjects_{task}.npz', subjects)
+
+"""
+Reading data back out:
+
+load = np.load(f'/scratch/users/csiyer/decoding_outputs/srm_data_{task}.npz')
+data_srm = [load[k] for k in load]
+load = np.load(f'/scratch/users/csiyer/decoding_outputs/labels_{task}.npz')
+labels = [load[k] for k in load]
+subjects = np.load(f'/scratch/users/csiyer/decoding_outputs/subjects_{task}.npz')
+"""
