6c3a7ff65f77cc1feaa57930a93520afd399f9bc
unknown <chris.s.iyer@gmail.com>
2024-04-02 19:37:44 -0700

    serialize beta map extraction

--- task_beta_mapping.py
+++ task_beta_mapping.py
@@ -7,7 +7,7 @@ Author: Chris Iyer
 Updated: 4/2/24
 """
 
-import glob
+import os, glob
 import numpy as np
 import pandas as pd
 from nilearn.maskers import MultiNiftiMasker
@@ -17,127 +17,111 @@ from joblib import Parallel, delayed
 from connectivity import get_combined_mask
 # /oak/stanford/groups/russpold/data/network_grant/discovery_BIDS_21.0.1/derivatives/glm_data_MNI
 
+
 def load_files(task):
     """
-    Retrieves data filenames, event files, and trimmed confounds dataframes given a task
+    Retrieves filenames of data, events, confounds. Saves list of subjects for later use. 
     """
     bids_dir = '/oak/stanford/groups/russpold/data/network_grant/discovery_BIDS_21.0.1/derivatives/glm_data_MNI'
     data_files = [f for f in glob.glob(bids_dir + f'/**/*{task}*optcom_bold.nii.gz', recursive=True) if 'ses-11' not in f and 'ses-12' not in f] # previously optcomDenoised
-    confound_files = [f for f in glob.glob(bids_dir + f'/**/*{task}*confounds*', recursive=True) if 'ses-11' not in f and 'ses-12' not in f]
-
-    def process_confound_files(data_files, confound_files): # make sure they correspond to data files, extract to pd df
-        confound_dfs = []
-        for d in data_files: # need to index on the data because there are some wonky ones with a confound file but no data file
-            sub_ses = d[d.find('sub'):d.find('sub')+14]
-            c_df = pd.read_csv([f for f in confound_files if sub_ses in f][0], sep='\t')
-            c_df = c_df[[col for col in c_df.columns if 'cosine' in col or 'trans' in col or 'rot' in col]] # just get cosine and 24 motion regressors
-            confound_dfs.append(c_df)
-        return confound_dfs
-    confounds = process_confound_files(data_files, confound_files)
-
-    events = [pd.read_csv(f, sep='\t') for f in glob.glob(bids_dir + f'/**/*{task}*events*', recursive=True) if 'ses-11' not in f and 'ses-12' not in f] 
     subjects = [e[e.find('sub') : e.find('sub')+7] for e in data_files]
+    event_files = [f for f in glob.glob(bids_dir + f'/**/*{task}*events*', recursive=True) if 'ses-11' not in f and 'ses-12' not in f] 
+    np.save(f'/scratch/users/csiyer/glm_outputs/{task}_subjects.npy', subjects)
+    confounds_files = [f for f in glob.glob(bids_dir + f'/**/*{task}*confounds*', recursive=True) if 'ses-11' not in f and 'ses-12' not in f]
 
-    return data_files, events, confounds, subjects
+    # a couple erroneous / unmatched confounds files to exclude
+    all_subjects_sessions = [d[d.find('sub'):d.find('sub')+14] for d in data_files]
+    confounds_files = [c for c in confounds_files if c[c.find('sub'):c.find('sub')+14] in all_subjects_sessions]
+
+    return subjects, data_files, event_files, confounds_files
 
 
-def glm_lsa(data_files, events, confounds, subjects, glm_params):
+def glm_lsa(sub, d_file, events, confounds, glm_params):
     """Trial-wise GLM modeling, using the Least Squares - All approach.
     Returns:
-        - a list (of length n_sessions), where each item is itself a list (of length n_trials) of beta maps. 
-        - a list (n_sessions) of lists (n_trials) of label tuples of the format (trial_type, 'correct/incorrect')
+        - a list (of length n_trials) of beta maps (nifti objects) 
+        - a list (n_trials) of label tuples of the format (trial_type, 'correct/incorrect')
     """
-    beta_maps = []
-    labels = []
+    beta_series = []
+    label_series = []
+    glm_params['subject_label'] = sub
 
-    for i_sub, sub in enumerate(subjects):
-        sub_beta_maps = []
-        sub_labels = []
-        glm_params['subject_label'] = sub
+    # construct new events df with trial-specific labels
+    lsa_events_df = events.copy()
+    conditions = lsa_events_df['trial_type'].unique()
+    condition_counter = {c: 0 for c in conditions}
+    for i_trial, trial in lsa_events_df.iterrows():
+        trial_condition = trial['trial_type']
+        condition_counter[trial_condition] += 1
+        lsa_events_df.loc[i_trial, 'trial_type'] = f"{trial_condition}__{condition_counter[trial_condition]:03d}" # new trial-specific label with '__'
+    
+    # fit glm with new events df
+    lsa_glm = FirstLevelModel(**glm_params)
+    lsa_glm.fit(d_file, events=lsa_events_df[['onset','duration','trial_type']], confounds=confounds)
+    
+    # extract beta maps
+    for i_trial,trial in lsa_events_df.iterrows():
+        beta_map = lsa_glm.compute_contrast(trial['trial_type'], output_type='effect_size') 
+        beta_series.append(beta_map)
 
-        # construct new events df with trial-specific labels
-        lsa_events_df = events[i_sub].copy()
-        conditions = lsa_events_df['trial_type'].unique()
-        condition_counter = {c: 0 for c in conditions}
-        for i_trial, trial in lsa_events_df.iterrows():
-            trial_condition = trial['trial_type']
-            condition_counter[trial_condition] += 1
-            lsa_events_df.loc[i_trial, 'trial_type'] = f"{trial_condition}__{condition_counter[trial_condition]:03d}" # new trial-specific label '__'
-        
-        # fit glm with new events df
-        lsa_glm = FirstLevelModel(**glm_params)
-        lsa_glm.fit(data_files[i_sub], events=lsa_events_df[['onset','duration','trial_type']], confounds=confounds[i_sub])
-        
-        # extract beta maps
-        for i_trial,trial in lsa_events_df.iterrows():
-            beta_map = lsa_glm.compute_contrast(trial['trial_type'], output_type='effect_size') 
-            sub_beta_maps.append(beta_map)
-            sub_labels.append( ( trial['trial_type'].split('__')[0], trial['correct_response'] == trial['key_press'] ) ) # original trial type and trial outcome
+        trial_type = trial['trial_type'].split('__')[0]
+        correct = trial['correct_response'] == trial['key_press']
+        label_series.append( (trial_type, correct) ) 
 
-        beta_maps.append(concat_imgs(sub_beta_maps))
-        labels.append(sub_labels)
-
-    return beta_maps, labels
+    return concat_imgs(beta_series), label_series
 
 
-def glm_lss(data_files, events, confounds, subjects, glm_params):
+def glm_lss(sub, d_file, events, confounds, glm_params):
     """ NOTE: NOT FUNCTIONAL YET (or efficient enough to actually run? idk)
     Trial-wise GLM modeling, using the Least Squares - Separate approach.
     Returns:
-        - a list (of length n_sessions), where each item is itself a list (of length n_trials) of beta maps. 
+        - a list (of length n_trials) of beta maps. 
         - a list of label tuples of the format (trial_type, 'correct/incorrect')
     """
-    beta_maps = []
-    labels = []
+    beta_series = []
+    label_series = []
+    glm_params['subject_label'] = sub
+    glm_params['n_jobs'] = 1 # parallelizing on our own
 
-    def label_one_row(df, row_number):
+    def relabel_one_row(df, row_number):
         """Label one trial for one LSS model. Takes events file and row number of trial to model."""
         df = df.copy()
-
         # Determine which number trial it is *within its condition*
         trial_condition = df.loc[row_number, "trial_type"]
         trials_of_this_type_indices = df["trial_type"].loc[df["trial_type"] == trial_condition].index.tolist()
         trial_number = trials_of_this_type_indices.index(row_number)
-
-        trial_name = f"{trial_condition}__{trial_number:03d}" # new trial-specific label
+        trial_name = f"{trial_condition}__{trial_number:03d}" # make new trial-specific label
         df.loc[row_number, "trial_type"] = trial_name
         return df, trial_name
     
-    def one_sub(i_sub):
-        sub = subjects[i_sub]
-        sub_beta_maps = []
-        sub_labels = []
-        glm_params['subject_label'] = sub
+    def glm_one_trial(i_trial):
+        lss_events_df, trial_condition = relabel_one_row(events, i_trial)
+        lss_glm = FirstLevelModel(**glm_params)
+        lss_glm.fit(d_file, events=lss_events_df[['onset','duration','trial_type']], confounds=confounds)
+        beta_map = lss_glm.compute_contrast(trial_condition, output_type="effect_size")
+        
+        trial_type = trial_condition.split('__')[0]
+        correct = events.correct_response.iloc[i_trial] == events.key_press.iloc[i_trial] 
+        return beta_map, (trial_type, correct)
 
-        for i_trial in range(len(events[i_sub])):
-            lss_events_df, trial_condition = label_one_row(events[i_sub], i_trial)
-
-            lss_glm = FirstLevelModel(**glm_params)
-            lss_glm.fit(data_files[i_sub], events=lss_events_df[['onset','duration','trial_type']], confounds=confounds[i_sub])
-            beta_map = lss_glm.compute_contrast(trial_condition, output_type="effect_size")
-
-            sub_beta_maps.append(beta_map)
-            sub_labels.append( (trial_condition.split('__')[0], events[i_sub]['correct_response'] == events[i_sub]['key_press']) )# original trial name + trial outcome
-
-        return concat_imgs(sub_beta_maps), sub_labels
-
-    out = Parallel(n_jobs = min(len(subjects), 32) ) (
-        delayed(one_sub)(i_sub) for i_sub in range(len(subjects))
+    out = Parallel(n_jobs=32) (
+        delayed(glm_one_trial)(i_trial) for i_trial in range(len(events)) 
     )
 
-    beta_maps = [s[0] for s in out]
-    labels = [s[1] for s in out]
-    return beta_maps, labels
+    beta_series = [s[0] for s in out]
+    label_series = [s[1] for s in out]
+
+    return concat_imgs(beta_series), label_series
 
 
-def extract_beta_series(data_files, events, confounds, subjects, method='LSA'):
+def extract_save_beta_maps(task, method='LSA'):
     """
-    For a given task, this function will run first level GLM to extract trial-wise beta timeseries (LSS or LSA)
-        for each subject/session. It will then vectorize the beta maps with nilearn maskers.
-    Returns:
-        data: a list (of length n_sessions) of beta series data from each trial
-        labels: a list (of length n_sessions) of lists (of n_trials) of tuples: (trial_type, <correct: True or False>)
+    For a given task, this function will load necessary files and run
+    first level GLM to extract trial-wise beta timeseries (LSS or LSA)
+    for each subject/session. It will then vectorize the maps with nilearn maskers and save as .npy files. 
     """
+    subjects, data_files, event_files, confounds_files = load_files(task)
+
     glm_params = { # SHOULD PULL THESE SPECIFICALLY FROM SOMEWHERE?
         't_r': 1.49,
         'mask_img': get_combined_mask(),
@@ -148,27 +132,50 @@ def extract_beta_series(data_files, events, confounds, subjects, method='LSA'):
         'n_jobs': 32
     }
 
-    if method=='LSA':
-        beta_maps, labels = glm_lsa(data_files, events, confounds, subjects, glm_params)
-    elif method=='LSS':
-        beta_maps, labels = glm_lss(data_files, events, confounds, subjects, glm_params)
+    for sub, d_file, e_file, c_file in zip(subjects, data_files, event_files, confounds_files):
+        
+        subject_session = d_file[d_file.find('sub'):d_file.find('sub')+14]
+        events = pd.read_csv(e_file, sep='\t')
+        confounds = pd.read_csv(c_file, sep='\t')
+        confounds = confounds[[col for col in confounds.columns if 'cosine' in col or 'trans' in col or 'rot' in col]] # just get cosine and 24 motion regressors
 
-    data = MultiNiftiMasker(
-        mask_img = get_combined_mask(), # mask where gray matter above 50% and the parcellation applies
-        standardize = 'zscore_sample',
-        n_jobs = 32
-    ).fit_transform(beta_maps)
+        if method=='LSA':
+            session_beta_maps, session_labels =  glm_lsa(sub, d_file, events, confounds, glm_params)
+        elif method=='LSS':
+            session_beta_maps, session_labels =  glm_lss(sub, d_file, events, confounds, glm_params)
 
-    return data, labels
+        session_beta_maps_masked = MultiNiftiMasker(
+            mask_img = get_combined_mask(), # mask where gray matter above 50% and the parcellation applies
+            standardize = 'zscore_sample',
+            n_jobs = 32
+        ).fit_transform(session_beta_maps)
+
+        np.save(f'/scratch/users/csiyer/glm_outputs/{task}_{subject_session}_beta_maps.npy', session_beta_maps_masked)
+        np.save(f'/scratch/users/csiyer/glm_outputs/{task}_{subject_session}_labels.npy', session_labels)
+        
+        del session_beta_maps, session_beta_maps_masked, session_labels
+
+
+def aggregate_saved_maps(task):
+    """Searches through saved beta maps + labels files and combines within each task."""
+    
+    GLM_PATH = f'/scratch/users/csiyer/glm_outputs/task'
+    task_beta_files = glob.glob(GLM_PATH + '*beta*')
+    task_beta_maps = [np.load(f) for f in task_beta_files]
+    np.save(f'/scratch/users/csiyer/glm_outputs/{task}_beta_maps.npy', task_beta_maps)
+
+    task_labels_files = glob.glob(GLM_PATH + '*labels*')
+    task_labels = [np.load(f) for f in task_labels_files]
+    np.save(f'/scratch/users/csiyer/glm_outputs/{task}_labels.npy', task_labels)
+    
+    for b,l in zip(task_beta_files, task_labels_files): # now delete old files
+        os.remove(b)
+        os.remove(l)
 
 
 if __name__ == "__main__":
     tasks = ['stopSignal','nBack','directedForgetting','goNogo','shapeMatching','spatialTS','cuedTS','flanker']
     for task in tasks:
-        data_files, events, confounds, subjects = load_files(task)
-        data, labels = extract_beta_series(data_files, events, confounds, subjects, method='LSA')
-        np.save(f'/scratch/users/csiyer/glm_outputs/{task}_beta_maps.npy', data)
-        np.save(f'/scratch/users/csiyer/glm_outputs/{task}_labels.npy', labels)
-        np.save(f'/scratch/users/csiyer/glm_outputs/{task}_subjects.npy', subjects)
+        extract_save_beta_maps(task, method='LSA')
+        aggregate_saved_maps(task)
         print(f'completed {task}')
-        del data, data_files, events, confounds, subjects, labels
