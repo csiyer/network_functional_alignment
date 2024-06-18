a55947dd4bbb1bed099767adebcd47a959afdc0f
unknown <chris.s.iyer@gmail.com>
2024-06-18 13:42:12 -0700

    conjunction analysis loso version!

--- contrast_map_checks/conjunction_analysis.py
+++ contrast_map_checks/conjunction_analysis.py
@@ -23,27 +23,37 @@ import numpy as np
 from scipy.stats import pearsonr
 import matplotlib.pyplot as plt
 from nilearn.maskers import NiftiMasker
-from utils import srm_brainiak as srm
 from joblib import Parallel, delayed
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from connectivity import get_combined_mask
-from srm import load_avg_connectomes, load_parcel_map
+from srm import load_parcel_map
+from utils.fastsrm_brainiak import FastSRM 
 
-CONTRAST_PATH = '/oak/stanford/groups/russpold/data/network_grant/discovery_BIDS_21.0.1/derivatives/output_optcom_MNI/'
-SRM_DIR = '/scratch/users/csiyer/srm_outputs/'
-srm_files = glob.glob(SRM_DIR + '*transform*')
 
-target_contrasts = {
-    'cuedTS': 'cuedTS_contrast-task_switch_cost',
-    'directedForgetting': 'directedForgetting_contrast-neg-con',
-    'flanker': 'flanker_contrast-incongruent - congruent',
-    'nBack': 'nBack_contrast-twoBack-oneBack',
-    'spatialTS': 'spatialTS_contrast-task_switch_cost',
-    'shapeMatching': 'shapeMatching_contrast-main_vars',
-    'goNogo': 'goNogo_contrast-nogo_success-go',
-    'stopSignal': 'stopSignal_contrast-stop_failure-go'
-}
+def load_all_filenames():
+    target_contrasts = {
+        'cuedTS': 'cuedTS_contrast-task_switch_cost',
+        'directedForgetting': 'directedForgetting_contrast-neg-con',
+        'flanker': 'flanker_contrast-incongruent - congruent',
+        'nBack': 'nBack_contrast-twoBack-oneBack',
+        'spatialTS': 'spatialTS_contrast-task_switch_cost',
+        'shapeMatching': 'shapeMatching_contrast-main_vars',
+        'goNogo': 'goNogo_contrast-nogo_success-go',
+        'stopSignal': 'stopSignal_contrast-stop_failure-go'
+    }
+    sub_files = {}
+    connectome_files = sorted(glob.glob('/scratch/users/csiyer/connectomes/*avg*'))
+    sub_list = [f[f.find('sub')+4:f.find('sub')+7] for f in connectome_files] # s03, s10, etc.
+
+    for s,c in zip(sub_list, connectome_files):
+        sub_files[s] = {'connectome': c}
+        for task in ['flanker','spatialTS','cuedTS','directedForgetting','stopSignal','goNogo', 'shapeMatching', 'nBack']:
+            CONTRAST_PATH = '/oak/stanford/groups/russpold/data/network_grant/discovery_BIDS_21.0.1/derivatives/output_optcom_MNI/'
+            full_task_fname = CONTRAST_PATH + f'{task}_lev1_output/task_{task}_rtmodel_rt_centered/contrast_estimates/'
+            sub_files[s][task] = glob.glob(full_task_fname + f'sub-{s}*{target_contrasts[task]}*t-test.nii.gz')[0]
+
+    return sub_files
 
 
 def mask(img_path):
@@ -54,11 +64,60 @@ def mask(img_path):
     ).fit_transform(img_path)[0,:] # just first slice (contrast maps are 2D)
 
 
-def srm_and_s1_native(map, sub_transform, s1_transform):
+def srm_and_loso_native(map, sub_transform, s1_transform):
     """Accepts a contrast map and which subject it belongs to, and dictionary of data. 
     Transforms the map into shared space and then back into the first subject's native space."""
-    srm_data = np.dot(map, sub_transform)
-    return np.dot(srm_data, s1_transform.T)
+    map_to_shared = np.dot(map, sub_transform)
+    return np.dot(map_to_shared, s1_transform.T)
+
+
+def compute_loso_srm(data_list, sub_list, loso_sub, parcel_map, n_features=100, save=True):
+    """
+    This function uses BrainIAK's Shared Response Modeling function to compute parcel-wise SRMs (one per parcel, as an anatomical constraint).
+    CRUCIALLY, the shared model is derived on all subjects but one; that left-out subject is subsequently added to derive their own transformation matrix.
+    This is done for the purpose of eliminating data leakage when we later transform all other subjects' data into that subject's native space.
+    
+    Inputs: list of connectomes, corresponding subject names, which subject to leave out, dimension-matched parcel map, shared model # dimensions, save?
+    Output: transformation matrices for each subject, in the order of sub_list and data_list inputs
+
+    NOTE: the parcelwise shared responses are not saved here (just the transformation matrices), because I haven't been using them for anything.
+    """
+    outpath = os.path.join('/scratch/users/csiyer/srm_outputs/', f'loso/{loso_sub}')
+    if os.path.exists(outpath): # if this script has been run before, we can load past results instead of re-deriving
+        return [np.load(glob.glob(f'{outpath}/sub-{sub}_srm_transform_loso-{loso_sub}.npy')[0]) for sub in sub_list]
+
+    loso_idx = sub_list.index(loso_sub)
+    loso_data = data_list[loso_idx]
+    train_data = [d for i,d in enumerate(data_list) if i != loso_idx]
+
+    temp_dir = '/scratch/users/csiyer/temp_dir'
+
+    def single_parcel_srm(train_data, loso_data, parcel_map, parcel_label, n_features):
+        parcel_idxs = np.where(parcel_map == parcel_label)
+        train_data_parcel = [d[parcel_idxs] for d in train_data]
+        srm = FastSRM(n_components=n_features, n_iter=20, n_jobs=1, aggregate='mean', temp_dir = temp_dir)
+        reduced_sr = srm.fit_transform(train_data_parcel)
+        srm.aggregate = None
+        srm.add_subjects([loso_data], reduced_sr)
+        return [np.load(x) for x in srm.basis_list], parcel_idx # return list of all the transforms, which are saved to temp_dir
+
+    srm_outputs = Parallel(n_jobs=-1)(
+        delayed(single_parcel_srm)(train_data, loso_data, parcel_map, parcel_label, n_features) for parcel_label in np.sort(np.unique(parcel_map))
+    )
+
+    subject_transforms = [np.zeros((len(parcel_map), n_features)) for _ in range(len(sub_list))] # empty initalize
+    for sub_weights, parcel_idx in srm_outputs: # for each parcel
+        for i,sub in enumerate(subject_transforms): # for each subject
+            sub[parcel_idx,:] = sub_weights[i] # add those parcel's transformation values to that subject's transformation matrix
+
+    # we need to reorder them to match the original sub_list. the loso_sub's transform is at the end, and we'll insert it at its original index
+    subject_transforms.insert(loso_idx, subject_transforms.pop())
+
+    if save:
+        for sub, transform in zip(sub_list, subject_transforms):
+            np.save(f'{outpath}/sub-{sub}_srm_transform_loso-{loso_sub}.npy', transform)
+
+    return subject_transforms
 
 
 def dice_coef(map1, map2, threshold_val=2):
@@ -91,51 +150,57 @@ def dice_coef(map1, map2, threshold_val=2):
     return dice
 
 
-def run_conjunction_analysis(threshold_val=2, save=True, savetag=''):
+def run_conjunction_analysis(srm_n_features=100, threshold_val=2, save=True, savetag=''):
     """
-    Main conjunction analysis script. Loops through contrast files, quantifies overlap and dice coefficients, 
-    and saves output to json. 
+    Main conjunction analysis script. 
+    Loops through the subjects, for each one:
+        - derives SRM transforms into that subject's native space (leaving that subject out from the initial SRM)
+        - transforms contrast maps into that native space
+        - quantify between-subject overlap in both transformed and un-transformed maps
+    plots output and saves results to .json file
     """
-    results = {}
+    sub_files = load_all_filenames() # sub_files['s3']['connectome'] = <path>;  sub_files['s3']['flanker'] = <path>
+    parcel_map = load_parcel_map(n_dimensions = 100)
+    results = {} 
+    
     for task in ['flanker','spatialTS','cuedTS','directedForgetting','stopSignal','goNogo', 'shapeMatching', 'nBack']:
         results[task] = {
-            'srm': {'all_r': [], 'all_dice': []},
-            'nosrm': {'all_r': [], 'all_dice': []}
+            'srm': {'r': [], 'dice': []}, 
+            'no_srm': {'r': [], 'dice': []}
         }
 
-        subjects = np.unique(np.load(f'/scratch/users/csiyer/glm_outputs/{task}_subjects.npy'))
-        full_task_fname = CONTRAST_PATH + f'{task}_lev1_output/task_{task}_rtmodel_rt_centered/contrast_estimates/'
+        for loso_idx,loso_sub in enumerate(sub_files.keys()): # leave one subject out of shared model creation, transform maps into their native space
 
-        # construct dictionary of all maps to be compared
-        sub_dict = {}
-        s1_transform = np.load(sorted(srm_files)[0]) # first subject's srm transform, to put all SRM'd data back in this native space
-        for sub in subjects:
-            contrast_map = mask(glob.glob(full_task_fname + f'{sub}*{target_contrasts[task]}*t-test.nii.gz')[0])
-            srm_transform = np.load([s for s in srm_files if sub in s][0])
-            sub_dict[sub] = {
-                'contrast_map': contrast_map,
-                'contrast_map_srm': srm_and_s1_native(contrast_map, srm_transform, s1_transform)
-            }
+            sub_list = [s for s in sub_files.keys() if s != loso_sub]
+            data_list = [np.load(v['connectome']) for s,v in sub_files.items() if s != loso_sub]
 
-        for sub1, sub2 in itertools.combinations(subjects, 2): # for each possible pair, compare maps
+            sub_transforms = compute_loso_srm(data_list, sub_list, loso_sub, parcel_map, n_features=srm_n_features, save=save)
+            contrast_maps = [mask(sub_files[s][task]) for s in sub_list]
+            contrast_maps_srm = [
+                m if s == loso_sub else srm_and_loso_native(m, t, sub_transforms[loso_idx])
+                for s, m, t in zip(sub_list, contrast_maps, sub_transforms)
+            ]
 
-            ### No SRM version
-            dice = dice_coef(sub_dict[sub1]['contrast_map'], sub_dict[sub2]['contrast_map'], threshold_val = threshold_val)
-            r, _ = pearsonr(sub_dict[sub1]['contrast_map'], sub_dict[sub2]['contrast_map'])
-            results[task]['nosrm']['all_r'].append(r)
-            results[task]['nosrm']['all_dice'].append(dice)
+            # now we have contrast maps that are both transformed and untransformed, and just need to quantify overlap for both
+            # to do so we will get the average dice coefficient between every possible pair of maps
+            pairs = itertools.combinations(range(len(sub_list)), 2)
+            results[task]['no_srm']['dice'].append(
+                [dice_coef(contrast_maps[i], contrast_maps[j], threshold_val = threshold_val) for i,j in pairs]
+            )
+            results[task]['srm']['dice'].append(
+                [dice_coef(contrast_maps_srm[i], contrast_maps_srm[j], threshold_val = threshold_val) for i,j in pairs]
+            )
+            results[task]['no_srm']['r'].append(
+                [pearsonr(contrast_maps[i], contrast_maps[j])[0] for i,j in pairs]
+            )
+            results[task]['srm']['r'].append(
+                [pearsonr(contrast_maps_srm[i], contrast_maps_srm[j])[0] for i,j in pairs]
+            )
 
-            ### SRM version
-            dice = dice_coef(sub_dict[sub1]['contrast_map_srm'], sub_dict[sub2]['contrast_map_srm'], threshold_val = threshold_val)
-            r, _ = pearsonr(sub_dict[sub1]['contrast_map_srm'], sub_dict[sub2]['contrast_map_srm'])
-            results[task]['srm']['all_r'].append(r)
-            results[task]['srm']['all_dice'].append(dice)
+        # now, we want to average across the left-out subjects
+        for method, measure in itertools.product(['srm','no_srm'], ['dice', 'r']):
+            results[task][method][measure] = np.mean(results[task][method][measure], axis=0) 
 
-        # average results
-        for method in ['srm', 'nosrm']: # get averages
-            results[task][method]['avg_r'] = np.mean(results[task][method]['all_r'])
-            results[task][method]['avg_dice'] = np.mean(results[task][method]['all_dice'])
-        
     if save: 
         OUTPATH = '/scratch/users/csiyer/conjunction_analysis/'
         if not os.path.isdir(OUTPATH):
@@ -152,26 +217,26 @@ def plot_results(results, save=True, savetag=''):
     
     # Subplot for Correlations
     axes[0].set_title("Contrast Map Correlation")
-    axes[0].set_ylabel("Average Pearson r")
+    axes[0].set_ylabel("Average Pearson r (across pairs of subjects)")
     axes[0].set_xticks(range(len(tasks)))
     axes[0].set_xticklabels(tasks, rotation=45, ha='right')
     
     # Subplot for Dice Coefficients
     axes[1].set_title("Contrast Map Dice Coefficients")
-    axes[1].set_ylabel("Average Dice Coefficient")
+    axes[1].set_ylabel("Average Dice Coefficient (across pairs of subjects)")
     axes[1].set_xticks(range(len(tasks)))
     axes[1].set_xticklabels(tasks, rotation=45, ha='right')
     
     # Plot data
     for i, task in enumerate(tasks):
-        axes[0].bar(i - 0.2, results[task]['srm']['avg_r'], yerr=np.std(results[task]['srm']['all_r']), 
+        axes[0].bar(i - 0.2, np.mean(results[task]['srm']['r']), yerr=np.std(results[task]['srm']['r']), 
                     width=0.4, label='SRM' if i == 0 else "", color='green', capsize=5, alpha = 0.5)
-        axes[0].bar(i + 0.2, results[task]['nosrm']['avg_r'], yerr=np.std(results[task]['nosrm']['all_r']), 
+        axes[0].bar(i + 0.2, np.mean(results[task]['no_srm']['r']), yerr=np.std(results[task]['no_srm']['r']), 
                     width=0.4, label='No SRM' if i == 0 else "", color='blue', capsize=5, alpha = 0.5)
         
-        axes[1].bar(i - 0.2, results[task]['srm']['avg_dice'], yerr=np.std(results[task]['srm']['all_dice']), 
+        axes[1].bar(i - 0.2, np.mean(results[task]['srm']['dice']), yerr=np.std(results[task]['srm']['dice']), 
                     width=0.4, label='SRM' if i == 0 else "", color='green', capsize=5, alpha = 0.5)
-        axes[1].bar(i + 0.2, results[task]['nosrm']['avg_dice'], yerr=np.std(results[task]['nosrm']['all_dice']), 
+        axes[1].bar(i + 0.2, np.mean(results[task]['no_srm']['dice']), yerr=np.std(results[task]['no_srm']['dice']), 
                     width=0.4, label='No SRM' if i == 0 else "", color='blue', capsize=5, alpha = 0.5)
 
     axes[0].legend()
@@ -185,6 +250,6 @@ def plot_results(results, save=True, savetag=''):
 
 
 if __name__ == '__main__':
-    results = run_conjunction_analysis()
+    results = run_conjunction_analysis(srm_n_features=100, threshold_val=2, save=True, savetag = 'loso_version')
     plot_results(results)
     
